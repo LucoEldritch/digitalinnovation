91d522cb840513bda38480c0ef29a9fac2a4c227
Luciano Amado <gas.luciano@hotmail.com>
2021-08-09 05:32:54 -0300

    Conteúdo da Classe Pessoa (aula OO parte 2)

--- src/main/kotlin/one/digitalinnovation/digionebank_la/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank_la/Pessoa.kt
@@ -1,5 +1,15 @@
 package one.digitalinnovation.digionebank_la
 
 class Pessoa {
+    var nome: String = "Jether"
+    var cpf: String = "123.456.789-01"
 
+    override fun toString(): String {
+        return "${this.nome}, ${this.cpf}"
+    }
+}
+
+fun main() {
+    val jether = Pessoa()
+    println(jether)
 }
